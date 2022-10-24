05f605f2e91ee9598ea7ca81490eeb1160172606
Ertugrul Dagli <ertugruldagl@gmail.com>
2022-10-24 16:32:22 +0300

    playerName commit

--- MaceraOyunu/src/Macera/Game.java
+++ MaceraOyunu/src/Macera/Game.java
@@ -1,4 +1,15 @@
 package Macera;
 
+import java.util.Scanner;
+
 public class Game {
+
+    private Scanner input =new Scanner(System.in);
+
+    public void start(){
+        System.out.println("Oyunumuza hosgeldiniz!");
+        System.out.print("Lutfen bir isim giriniz: ");
+        String playerName=input.next();
+
+    }
 }
